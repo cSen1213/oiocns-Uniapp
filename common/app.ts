3b0c3d8c34adf841f983cbc984923127b186149e
SEN <changsen25@163.com>
2023-06-12 14:18:35 +0800

    running

--- common/app.ts
+++ common/app.ts
@@ -4,43 +4,41 @@ import { AppConfig, ConfigurationManager } from "@orginone/core/lib/config";
 import { Store, StateAction } from "@orginone/core/lib/state";
 import { IStorage } from "@orginone/core/lib/storage/Storage";
 import { ShallowRefState } from "@orginone/vue/lib/ShallowRefState";
-import AccountApi from '@orginone/core/lib/lib/api/account'
-import KernelApi from '@orginone/core/lib/lib/api/kernelapi'
-import AnyStoreApi from '@orginone/core/lib/lib/api/anystore'
+import AccountApi from "@orginone/core/lib/lib/api/account";
+import KernelApi from "@orginone/core/lib/lib/api/kernelapi";
+import AnyStoreApi from "@orginone/core/lib/lib/api/anystore";
 import { OrginoneServices } from "@orginone/core";
-import  { UniappRuntime }  from "@orginone/runtime-uniapp";
+import { UniappRuntime } from "@orginone/runtime-uniapp";
 import { AuthorizationStore } from "@orginone/core/lib/lib/store/authorization";
 
-const config = new ConfigurationManager<AppConfig>()
-  .addConfig({
-    apiUrl: "https://orginone.cn/orginone"
-  });
+const config = new ConfigurationManager<AppConfig>().addConfig({
+  apiUrl: "https://orginone.cn/orginone",
+});
 // 注册服务
 const builder = new ServiceBuilder()
   .use(OrginoneServices)
-  .use(UniappRuntime as any,uni)
+  .use(UniappRuntime as any, uni)
   .factory(ConfigurationManager<AppConfig>, ctx => config)
   .instance<StateAction>("StateAction", ShallowRefState);
 // 生成服务容器并创建应用
 const services = builder.build();
 const app = App.create({
   config,
-  services
+  services,
 });
 // 启动应用
 app.start();
-console.log(app);
+console.log("app", app, AccountApi, KernelApi, AnyStoreApi);
 
-let accountApi =  app.services.resolve(AccountApi)
+let accountApi = app.services.resolve(AccountApi);
 
-let kernelApi  =  app.services.resolve(KernelApi)
+let kernelApi = app.services.resolve(KernelApi);
 
-let anyStoreApi   =  app.services.resolve(AnyStoreApi)
+let anyStoreApi = app.services.resolve(AnyStoreApi);
 
-let storage    =  app.services.resolve<IStorage>("IStorage");
+let storage = app.services.resolve<IStorage>("IStorage");
 
-let store      =  app.services.resolve<Store<AuthorizationStore>>("AuthorizationStore");
+let store =
+  app.services.resolve<Store<AuthorizationStore>>("AuthorizationStore");
 
-export {
-	app,accountApi,kernelApi,storage,store,anyStoreApi
-}
+export { app, accountApi, kernelApi, storage, store, anyStoreApi };
